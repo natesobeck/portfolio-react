2189f1ea932691ef87dd043633211fc780780126
Nate Sobeck <nsobeck910@gmail.com>
2024-03-24 15:47:14 -0400

    adjust link to deployed projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item" key={item.name}>
                   <div className="item-wrap">
-                    <a href={item.url}>
+                    <a href={item.url} target='_blank' rel='noreferrer'>
                       <img src={`${item.imgurl}`} className="item-img" alt={`the ${item.name} app`}/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
