48b3e9a61862b644572a7892de6b59236fe3566e
Nate Sobeck <nsobeck910@gmail.com>
2024-03-24 14:21:52 -0400

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,126 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Nate Sobeck",
+    "role": "Full Stack Developer with a Frontend Focus",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I've always been fascinated by skills without shortcuts, where consistency and dedication are the only path to success. I use my natural inclination for skill development to improve people's lives through sleek, user-friendly and robust web applications.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/n-sobeck/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/natesobeck",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"email",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-email"
+        }
+      ],
+    "aboutme":"Ever since I was a little kid, I've always loved building things. Every holiday season I'd get excited for another set of complicated legos. I loved the process of seeing your creation come to life. These days, the things that I build may be different but the passion is all the same. As I grew older, I became increasingly interested in tech until I finally decided to dive in and become a software engineer. The passion that I felt as a kid has never dwindled, but now my excitement is directed towards creating intuitive UX designs and robust web applications. When I'm not coding, you can find me working on my other passions. I am an avid lifting enthusiast, a chess hobbyist, a guitar player and singer, and a big video game fan. I live with my wonderful girlfriend and two cats.",
+    "address":"Boston, MA",
+    "website":"nsobeck910@gmail.com",
+    "education":[
+      {
+        "UniversityName":"General Assembly",
+        "specialization":"Full Stack Software Engineering Immersive",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        "Achievements":"Full-stack software engineering immersive student in an intensive, 24-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developed a portfolio of individual and group projects."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Legal Sea Foods",
+        "specialization":"Bartender & Server",
+        "MonthOfLeaving":"Sep 2019 - Present",
+        "Achievements":"Maintained high quality of work in a high volume, high stress collaborative environment. Up to $5000 in sales daily. Fostered lasting relationships and a friendly environment through a positive disposition and keen attention to detail. Managed difficult guests and coworkers alike with respect and professionalism."
+      },
+      {
+        "CompanyName":"Commonwealth Sports Club",
+        "specialization":"Personal Trainer",
+        "MonthOfLeaving":"Jan 2020 - May 2022",
+        "Achievements":"Created and implemented dozens of custom training plans to help people meet individual goals and ensure continuous progress. Communicated abstract training concepts to 150+ individuals in a digestible and accessible manner."
+      },
+      {
+        "CompanyName":"Spectrum Health Systems",
+        "specialization":"Community Coordinator for Project Peer Connection",
+        "MonthOfLeaving":"Jan 2020 - May 2022",
+        "Achievements":"Project Peer Connection was a federal grant from the DOJ with a goal of reducing recidivism and helping clients who identify with having a substance abuse problem and are medium risk of violence or higher. Counseled inmates to determine goals and needs and created success plans for societal reintegration. Maintained ongoing relationships post incarceration for up to one year to encourage successful reentry."
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"NodeJs"
+      },
+      {
+        "skillname":"python"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"express"
+      },
+      {
+        "skillname":"mongo db"
+      },
+      {
+        "skillname":"mongoose"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"Docker"
+      }
+
+    ],
+    "portfolio":[
+      {
+        "name":"Waypoint",
+        "description":"Trip planning app made with MERN stack with JWT Auth",
+        "imgurl":"images/triplist.png"
+      },
+      {
+        "name":"Coffees & Posses",
+        "description":"Group community club organizer made with MERN stack with JWT Auth",
+        "imgurl":"images/coffee1.png"
+      },
+      {
+        "name":"Habitual",
+        "description":"Habit forming app made with Python and Django",  
+        "imgurl":"images/habitual1.png"
+      },
+      {
+        "name":"Muscle Memories",
+        "description":"My first full CRUD app. Program and exercise tracker. MongoDB/Mongoose backend and EJS for views with Google OAuth",
+        "imgurl":"images/musclememories1.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+    ]
+  }
+  
+  export default resumeData
